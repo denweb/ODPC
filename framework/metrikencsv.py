402e2635c325cd884d525b1761da983e3feb23dc
Dennis <d.weber@uni-duesseldorf.de>
2021-04-01 14:32:28 +0200

    Results in CSV Anfang

--- framework/metrikencsv.py
+++ framework/metrikencsv.py
@@ -11,9 +11,13 @@ from framework.portalEbene import get_portal_metriken
 from framework.metaEbene import get_meta_metriken
 from framework.rohEbene import get_roh_metriken
 from framework.framework import get_metadataids, get_rohdataids_list, get_datum_ids
+from framework.utility.lizenzen import nicht_offen
 from urllib.parse import urlparse
+from statistics import mean
+from ast import literal_eval
 
 
+# Todo: Fix Anteile > 1
 def gen_csv_metrik(db, portal, kontakte, akt_daten, dateiformate_ids, se_fehler, vollst_fehler):
     res = None
 
@@ -25,6 +29,9 @@ def gen_csv_metrik(db, portal, kontakte, akt_daten, dateiformate_ids, se_fehler,
     portal_domain = urlparse(db.get_attr_single("portal", "url", "portalID", portal)[0]["url"]).netloc
 
     datum_ids = get_datum_ids(db, meta)
+
+    for data in roh:
+        print(type(data["anzahlFehler"]))
 
     if meta:
         genau = get_genau(meta, kontakte, portal)
@@ -45,86 +52,91 @@ def gen_csv_metrik(db, portal, kontakte, akt_daten, dateiformate_ids, se_fehler,
             "Portalsoftware": 0,
             "DS": len(meta),
             "Roh_pro_DS": len(roh)/len(meta),
-            "Titel_Länge": 0,
-            "Beschreibung_DS": 0,
+            "Titel_Länge": mean([len(data["titel"]) for data in meta]),
+            "Beschreibung_DS": len([1 for data in meta if data["beschreibung"] != "None"]),
             "Beschreibung_DS_Anteil": 0,
-            "Beschreibung_Länge": 0,
+            "Beschreibung_Länge": mean([len(data["beschreibung"]) for data in meta if data["beschreibung"] != "None"]),
             "Kontakte_einz": 0,
             "Kontakte_valide_Anteil": 0,
             "DS_Kontakte_valide_Anteil": 0,
-            "URL_einz": 0,
-            "DS_URL": 0,
+            "URL_einz": len({data["url"] for data in meta if data["url"] != "None"}),
+            "DS_URL": len([1 for data in meta if data["url"] != "None"]),
             "DS_URL_Anteil": 0,
-            "DS_geo": 0,
+            "DS_geo": len([1 for data in meta if data["url"] != "None"]),
             "DS_geo_Anteil": 0,
-            "DS_Erstelldatum": 0,
+            "DS_Erstelldatum":  len([1 for data in meta if data["erstellDatum"] != 3]),
             "DS_Erstelldatum_Anteil": 0,
             "Erstelldatum_Zeitspanne": 0,
-            "DS_Updatedatum": 0,
+            "DS_Updatedatum": len([1 for data in meta if data["updateDatum"] != 3]),
             "DS_Updatedatum_Anteil": 0,
             "Updatedatum_Zeitspanne": 0,
-            "Updatedatum_ung_Erstelldatum": 0,
+            "Updatedatum_ung_Erstelldatum": len([1 for data in meta
+                                                 if all([data["updateDatum"] != 3, data["erstellDatum"] != 3,
+                                                         data["updateDatum"] != data["erstellDatum"]])]),
             "Updatedatum_ung_Erstelldatum_Anteil": 0,
-            "Tags": 0,
-            "Tags_einz": 0,
-            "DS_mit_Tags": 0,
+            "Tags": sum([len(literal_eval(data["tags"])) for data in meta]),
+            "Tags_einz": len({tag for data in meta for tag in literal_eval(data["tags"])}),
+            "DS_mit_Tags": len([1 for data in meta if literal_eval(data["tags"])]),
             "DS_mit_Tags_Anteil": 0,
             "Tags_pro_DS_Durchschnitt": 0,
-            "Gruppen": 0,
-            "Gruppen_einzig": 0,
-            "DS_mit_Gruppen": 0,
+            "Gruppen": sum([len(literal_eval(data["gruppe"])) for data in meta]),
+            "Gruppen_einzig": len({tag for data in meta for tag in literal_eval(data["gruppe"])}),
+            "DS_mit_Gruppen": len([1 for data in meta if literal_eval(data["gruppe"])]),
             "DS_mit_Gruppen_Anteil": 0,
             "Gruppen_pro_DS_Durchschnitt": 0,
-            "Lizenzen_einz": 0,
+            "Lizenzen_einz": len({data["lizenz"] for data in meta if data["lizenz"] != 2}),
             "Lizenzen_offen_Anteil": 0,
-            "DS_mit_Lizenz": 0,
+            "DS_mit_Lizenz": len([1 for data in meta if data["lizenz"] != 2]),
             "DS_mit_Lizenz_Anteil": 0,
-            "DS_mit_Lizenz_offen": 0,
+            "DS_mit_Lizenz_offen": len([1 for data in meta if data["lizenz"] not in nicht_offen]),
             "DS_mit_Lizenz_offen_Anteil": 0,
             "Roh": len(roh),
-            "Link_intern_Anteil": 0,
-            "online": 0,
+            "Link_intern_Anteil": len([1 for data in roh if portal_domain in data["link"]]) / len(roh),
+            "online": len([1 for data in roh if data["online"] == "True"]),
             "online_Anteil": 0,
             "offline": 0,
             "offline_Anteil": 0,
-            "Name_Anteil": 0,
-            "Name_einz": 0,
+            "Name_Anteil": len([1 for data in roh if data["dateiName"] != "None"]) / len(roh),
+            "Name_einz": len({data["dateiName"] for data in roh if data["dateiName"] != "None"}),
             "Name_einz_Länge": 0,
             "DS_Name_einz_Anteil": 0,
-            "valid_0": 0,
+            "valid_0": len([1 for data in roh if data["valide"] == 0]),
             "valid_0_Anteil": 0,
-            "valid_1": 0,
+            "valid_1": len([1 for data in roh if data["valide"] == 1]),
             "valid_1_Anteil": 0,
-            "valid_2": 0,
+            "valid_2": len([1 for data in roh if data["valide"] == 2]),
             "valid_2_Anteil": 0,
-            "valid_3": 0,
+            "valid_3": len([1 for data in roh if data["valide"] == 3]),
             "valid_3_Anteil": 0,
-            "valid_4": 0,
+            "valid_4": len([1 for data in roh if data["valide"] == 4]),
             "valid_4_Anteil": 0,
-            "valid_5": 0,
+            "valid_5": len([1 for data in roh if data["valide"] == 5]),
             "valid_5_Anteil": 0,
-            "Beschreibung_Roh": 0,
+            "Beschreibung_Roh": len([1 for data in roh if data["beschreibung"] != "None"]),
             "Beschreibung_Roh_Anteil": 0,
             "Beschreibung_Roh_Länge": 0,
             "Fehler_Durchschnitt": 0,
             "Fehler_pro_Roh_Durchschnitt": 0,
-            "Dateityp_einzig": 0,
+            "Dateityp_einz": len({data["dateiTyp"] for data in roh if data["dateiTyp"] != 1}),
             "Dateityp_gegeben_Anteil": 0,
+            "DateitypReal_einz": len({data["dateiTypReal"] for data in roh if data["dateiTypReal"] != 1}),
             "DateitypReal_gegeben_Anteil": 0,
-            "DateitypReal_einzig_Roh_Durchschnitt": 0,
-            "Roh_ML": 0,
+            "DateitypReal_einz_DS_Durchschnitt": get_df_mean(roh),
+            "Roh_ML": len([1 for data in roh if data["dateiTypReal"] in dateiformate_ids["dateiformate_mr"]]),
             "Roh_ML_Anteil": 0,
-            "Roh_offen": 0,
+            "Roh_offen": len([1 for data in roh if data["dateiTypReal"] in dateiformate_ids["dateiformate_np"]]),
             "Roh_offen_Anteil": 0,
-            "Roh_Erstelldatum": 0,
+            "Roh_Erstelldatum": len([1 for data in roh if data["erstellDatum"] != 3]),
             "Roh_Erstelldatum_Anteil": 0,
             "Roh_Erstelldatum_Zeitspanne": 0,
-            "Roh_Updatedatum": 0,
+            "Roh_Updatedatum": len([1 for data in roh if data["updateDatum"] != 3]),
             "Roh_Updatedatum_Anteil": 0,
             "Roh_Updatedatum_Zeitspanne": 0,
-            "Roh_Updatedatum_ung_Erstelldatum": 0,
+            "Roh_Updatedatum_ung_Erstelldatum": len([1 for data in roh
+                                                     if all([data["updateDatum"] != 3, data["erstellDatum"] != 3,
+                                                             data["updateDatum"] != data["erstellDatum"]])]),
             "Roh_Updatedatum_ung_Erstelldatum_Anteil": 0,
-            "Roh_validierbar": 0,
+            "Roh_validierbar": len([1 for data in roh if data["anzahlFehler"] != "None"]),
             "Roh_validierbar_Anteil": 0,
             "Fehler_validierbar_Durchschnitt": 0,
             "Rohdaten_Fehler_Zelle": 0,
@@ -144,6 +156,66 @@ def gen_csv_metrik(db, portal, kontakte, akt_daten, dateiformate_ids, se_fehler,
             "Dateigröße_max": 0,
             "Dateigröße_Summe": 0,
         }
+
+        # Anteile - Meta
+        res["Beschreibung_DS_Anteil"] = res["Beschreibung_DS"] / res["DS"]
+        res["DS_URL_Anteil"] = res["DS_URL"] / res["DS"]
+        res["DS_geo_Anteil"] = res["DS_geo"] / res["DS"]
+        res["DS_Erstelldatum_Anteil"] = res["DS_Erstelldatum"] / res["DS"]
+        res["DS_Updatedatum_Anteil"] = res["DS_Updatedatum"] / res["DS"]
+        res["Updatedatum_ung_Erstelldatum_Anteil"] = res["Updatedatum_ung_Erstelldatum"] / res["DS"]
+        res["DS_mit_Tags_Anteil"] = res["DS_mit_Tags"] / res["DS"]
+        res["Tags_pro_DS_Durchschnitt"] = res["Tags"] / res["DS"]
+        res["DS_mit_Gruppen_Anteil"] = res["DS_mit_Gruppen"] / res["DS"]
+        res["Gruppen_pro_DS_Durchschnitt"] = res["Gruppen"] / res["DS"]
+        if res["Lizenzen_einz"] != 0:
+            res["Lizenzen_offen_Anteil"] = len({data["lizenz"] for data in meta if data["lizenz"] not in nicht_offen}) \
+                                           / res["Lizenzen_einz"]
+        res["DS_mit_Lizenz_Anteil"] = res["DS_mit_Lizenz"] / res["DS"]
+        res["DS_mit_Lizenz_offen_Anteil"] = res["DS_mit_Lizenz_offen"] / res["DS"]
+
+        # Weiteres Roh
+        res["online_Anteil"] = res["online"] / res["Roh"]
+        res["offline"] = res["Roh"] - res["online"]
+        res["offline_Anteil"] = res["offline"] / res["Roh"]
+        try:
+            res["Name_einz_Länge"] = mean({len(data["dateiName"]) for data in roh if data["dateiName"] != "None"})
+        except Exception:
+            pass
+        res["DS_Name_einz_Anteil"] = res["Name_einz"] / res["Roh"]
+        res["valid_0_Anteil"] = res["valid_0"] / res["Roh"]
+        res["valid_1_Anteil"] = res["valid_1"] / res["Roh"]
+        res["valid_2_Anteil"] = res["valid_2"] / res["Roh"]
+        res["valid_3_Anteil"] = res["valid_3"] / res["Roh"]
+        res["valid_4_Anteil"] = res["valid_4"] / res["Roh"]
+        res["valid_5_Anteil"] = res["valid_5"] / res["Roh"]
+        res["Beschreibung_Roh_Anteil"] = res["Beschreibung_Roh"] / res["Roh"]
+        try:
+            res["Beschreibung_Roh_Länge"] = \
+                mean([len(data["beschreibung"]) for data in roh if data["beschreibung"] != "None"])
+        except Exception:
+            pass
+        try:
+            res["Fehler_Durchschnitt"] = mean([data["anzahlFehler"] for data in roh
+                                               if all([data["anzahlFehler"] != "None",
+                                                       data["anzahlFehler"] != 0])])
+        except Exception:
+            pass
+        try:
+            res["Fehler_pro_Roh_Durchschnitt"] = mean([data["anzahlFehler"] for data in roh
+                                                       if all([data["anzahlFehler"] != "None",
+                                                               data["anzahlFehler"] != 0])])
+        except Exception:
+            pass
+        res["Dateityp_gegeben_Anteil"] = len([1 for data in roh if data["dateiTyp"] != 1]) / res["Roh"]
+        res["DateitypReal_gegeben_Anteil"] = len([1 for data in roh if data["dateiTypReal"] != 1]) / res["Roh"]
+        res["Roh_ML_Anteil"] = res["Roh_ML"] / res["Roh"]
+        res["Roh_offen_Anteil"] = res["Roh_offen"] / res["Roh"]
+        res["Roh_Erstelldatum_Anteil"] = res["Roh_Erstelldatum"] / res["DS"]
+        res["Roh_Updatedatum_Anteil"] = res["Roh_Updatedatum"] / res["DS"]
+        res["Roh_Updatedatum_ung_Erstelldatum_Anteil"] = res["Roh_Updatedatum_ung_Erstelldatum"] / res["DS"]
+
+
         for framew in [
             (genau, "gen_"),
             (vollst, "vollst_"),
@@ -161,5 +233,22 @@ def gen_csv_metrik(db, portal, kontakte, akt_daten, dateiformate_ids, se_fehler,
             for metr in framew[0]:
                 if metr != "portalID":
                     res["".join([framew[1], metr])] = framew[0][metr]
+
+        print(res)
+
+    return res
+
+
+def get_df_mean(roh):
+    df_counter = {}
+    for data in roh:
+        if all([data["dateiTypReal"], data["dateiTypReal"] != 1]):
+            if data["metaDatensatz"] not in df_counter:
+                df_counter[data["metaDatensatz"]] = {data["dateiTypReal"]}
+            else:
+                df_counter[data["metaDatensatz"]].add(data["dateiTypReal"])
+
+    df = [len(df_counter[d]) for d in df_counter]
+    res = mean(df)
 
     return res
